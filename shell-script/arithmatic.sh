d4cad1336f03e003e87ee965118b58508ef8350a
manaranjan-fullstack <mardanamanaranjan@gmail.com>
2026-09-06 14:15:24 +0530

    arithmatic ope*n

---
+++ shell-script/arithmatic.sh
@@ -0,0 +1,10 @@
+# Arithmatic Operations in Shell Script
+
+read -p "Enter first number: " num1
+read -p "Enter second number: " num2
+read -p "Enter third number: " num3
+
+echo "addition of $num1,$num2 and $num3 is:: $((num1 + num2 + num3))"
+echo "subtraction of $num1,$num2 and $num3 is:: $(((num1 - num2) - num3))"
+echo "multiplication of $num1,$num2 and $num3 is:: $((num1 * num2 * num3))"
+echo "division of $num1,$num2 and $num3 is:: $(((num1 / num2) / num3))"
