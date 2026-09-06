8e283a4d91355b8c08d0f717c18f8a67f4622603
manaranjan-fullstack <mardanamanaranjan@gmail.com>
2026-09-06 12:57:22 +0530

    get input from user and understand the variable concept

---
+++ shell-script/variable.sh
@@ -0,0 +1,12 @@
+echo " VARIABLES IN SHELL SCRIPT "
+
+name="Manaranjan Mardana"
+roll=10
+echo "My name is $name and my roll number is $roll"
+
+echo "get input from user"
+
+read -p "Enter any text ::" text
+echo "The text you entered is:: $text"
+
+
