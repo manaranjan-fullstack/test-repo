20486459a0070ad397f9eb2250a8c8f3951af6cf
manaranjan-fullstack <mardanamanaranjan@gmail.com>
2026-09-06 12:38:34 +0530

    how to print or show output. i , mean print ststement

---
+++ shell-script/hellow.sh
@@ -0,0 +1,11 @@
+# this is the first shell script
+
+echo "Hello, I am learning shell scripting."
+echo -n "My name is Manaranjan Mardana"
+echo ""
+
+
+cat << EOF
+This is a multi-line string.
+I am learning shell scripting.
+EOF
