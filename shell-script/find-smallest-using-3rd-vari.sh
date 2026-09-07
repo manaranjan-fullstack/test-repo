e89a78814ebcb27cfbde2718cfdcc2f90e555440
manaranjan-fullstack <mardanamanaranjan@gmail.com>
2026-09-07 22:04:10 +0530

    find the smallest among three intergers using 3rd variable 'small'

---
+++ shell-script/find-smallest-using-3rd-vari.sh
@@ -0,0 +1,16 @@
+# find the smallest among three intergers using 3rd variable 'small'
+
+read -p "Enter first number: " num1
+read -p "Enter second number: " num2
+read -p "Enter third number: " num3
+
+if [ "$num1" -lt "$num2" ] && [ "$num1" -lt "$num3" ]
+then
+    small=$num1
+elif [ "$num2" -lt "$num1" ] && [ "$num2" -lt "$num3" ]
+then
+    small=$num2
+else
+    small=$num3
+fi
+echo "The smallest number among $num1, $num2, and $num3 is: $small"
