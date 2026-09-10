f8104a1fab23afe89f352fd5f3df1ee6a069d911
manaranjan-fullstack <mardanamanaranjan@gmail.com>
2026-09-10 08:01:51 +0530

    change into pure unix command

--- shell-script/11.sh
+++ shell-script/11.sh
@@ -1,16 +1,23 @@
-
-# fibonacci series upto n
+#!/bin/sh
 
 echo "Enter a number to find fibonacci series upto n: "
 read n
+
 a=0
 b=1
+i=1
+
 echo "Fibonacci series upto $n: "
-for (( i=1; i <=n; i++ ))
-do 
-    echo -n "$a "
-    next_term=$((a+b))
+
+while [ $i -le $n ]
+do
+    printf "%d " "$a"
+
+    next_term=`expr $a + $b`
     a=$b
     b=$next_term
+
+    i=`expr $i + 1`
 done
-    
+
+echo
