94d6195dc39edd47ec3f7aa31c4424c5167bd880
manaranjan-fullstack <mardanamanaranjan@gmail.com>
2026-09-10 07:57:17 +0530

    # Find the smallest among three integers using 3rd variable 'small'

--- shell-script/5-find-smallest-using-3rd-vari.sh
+++ shell-script/5-find-smallest-using-3rd-vari.sh
@@ -1,16 +1,25 @@
-# find the smallest among three intergers using 3rd variable 'small'
+# Find the smallest among three integers using 3rd variable 'small'
 
 read -p "Enter first number: " num1
 read -p "Enter second number: " num2
 read -p "Enter third number: " num3
 
-if [ "$num1" -lt "$num2" ] && [ "$num1" -lt "$num3" ]
+if [ $num1 -lt $num2 ]
 then
     small=$num1
-elif [ "$num2" -lt "$num1" ] && [ "$num2" -lt "$num3" ]
-then
-    small=$num2
+
+    if [ $num3 -lt $small ]
+    then
+        small=$num3
+    fi
+
 else
-    small=$num3
+    small=$num2
+
+    if [ $num3 -lt $small ]
+    then
+        small=$num3
+    fi
 fi
-echo "The smallest number among $num1, $num2, and $num3 is: $small"
+
+echo "The Smallest number is: $small"
