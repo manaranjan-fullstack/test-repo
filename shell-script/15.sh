e30cd2967f35711d260be4451d88bcccb691b578
manaranjan-fullstack <mardanamanaranjan@gmail.com>
2026-09-12 12:01:14 +0530

    # shell script to concatinate two strings and find the total number of characters in the concatenated string and find the total number of characters in the resulring string.

---
+++ shell-script/15.sh
@@ -0,0 +1,9 @@
+# shell script to concatinate two strings and find the total number of characters in the concatenated string and find the total number of characters in the resulring string.
+
+read -p "WRITE A FIRST VALID STRING:: " s1
+read -p "WRITE A SECOND VALID STRING:: " s2
+
+result="$s1 $s2"
+echo "Concatenated string is: $result"
+len=$(expr length "$result")
+echo "Total number of characters in the concatenated string is:: $len"
